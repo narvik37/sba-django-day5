55c5d41a67fd65892b521392f9092e82edd83f6f
Kyuhyeon <narvik37@naver.com>
2021-01-20 13:39:43 +0900

    version 20210120

---
+++ members/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from .models import Members
+
+
+# Create your views here.
+def index(req):
+    return HttpResponse("Hello World");
+
+def test(req):
+    return HttpResponse("<h1>hello</h1>");
+def test2(req):
+    return HttpResponse("<h1>test2</h1>");
+def signup(req):
+    if req.method == 'POST':
+        username = req.POST['username']
+        email = req.POST['useremail']
+        #username == exit
+        # h2로 나가기를 출력
+        #if username == 'exit' :
+        #    return HttpResponse('나가기')
+        #elif username == 'codingon':
+        #    return render(req, 'login.html')
+
+        member = Members(
+            username = username,
+            useremail = email
+        )
+        
+        member.save()
+        res_data = {}
+        res_data['res'] = '등록성공'
+
+        return render(req, 'index.html', res_data)
+
+        print(req.POST['username'])
+    return render(req, 'index.html');
+
