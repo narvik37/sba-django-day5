6aab9d6acddf65c1e3754404b66613d6bfb23a19
Kyuhyeon <narvik37@naver.com>
2021-01-20 16:24:02 +0900

    lottos

---
+++ lottos/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+import random
+
+# Create your views here.
+def index(req):
+    lotto = []
+    while len(lotto) < 6:
+        lotto.append(random.randint(1, 46))
+        lotto = list(set(lotto))
+    return HttpResponse(f"<h1>lotto 번호 추천{lotto}</h1>")
